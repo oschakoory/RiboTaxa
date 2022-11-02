af243bcbb6d4e8eb4f0d089d9992fd4893214fac
Chakoory Oshma <oshma2803@gmail.com>
2022-11-02 08:39:58 +0100

    minor changes

--- conda_virt_env.sh
+++ conda_virt_env.sh
@@ -15,8 +15,8 @@ echo ""
 
 conda env create -n RiboTaxa_py27 --file RiboTaxa_py27_requirements.txt
 
-conda install -n RiboTaxa_py27 -c bioconda/label/cf201901 multiqc
+conda install -n RiboTaxa_py27 -c bioconda/label/cf201901 multiqc -y
 
 conda env create -n RiboTaxa_py36 --file RiboTaxa_py36_requirements.yml
 
-conda install -n RiboTaxa_py36 -c bioconda matam
+conda install -n RiboTaxa_py36 -c bioconda matam -y
